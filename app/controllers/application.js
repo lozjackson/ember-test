e0f40d09eff3c01c7d6820bd68a50d429164ce5f
Loz Jackson <lozjackson@talktalk.net>
2016-03-05 17:59:48 +0000

    update app

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
+  /*
+    This is a computed property that returns an `Ember.ObjectProxy` object.  The
+    `content` of the ObjectProxy is the `model`.
+  */
   obj: Ember.computed('model', function () {
     return Ember.ObjectProxy.create({
   		content: this.get('model')
--- tests/acceptance/application-test.js
+++ tests/acceptance/application-test.js
@@ -4,13 +4,25 @@ import Ember from 'ember';
 
 moduleForAcceptance('Acceptance | application');
 
-test('visiting /application', function(assert) {
+/*
+  This test passes in Ember v2.3.1
+  but fails in Ember v2.4.1
+*/
+test('obj.name should be the same as model.name', function(assert) {
   visit('/');
 
   andThen(function() {
     assert.equal(currentURL(), '/');
 
+    /*
+      `model` is an Ember.Object created on the route and set as the controller's `model`
+    */
     assert.equal(Ember.$('#model-name').text().trim(), 'model.name: abc');
+
+    /*
+      `obj` is an Ember.ObjectProxy with the `model` object set as its `content`
+      `obj.name` should be the same as `model.name`
+    */
     assert.equal(Ember.$('#obj-name').text().trim(), 'obj.name: abc');
   });
 });
